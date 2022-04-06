868186b34c45b85b73b53faab770ecf20238f0fa
Alexandr <drk91@inbox.ru>
2022-04-06 16:50:07 +0300

    Initial Commit

---
+++ vmeste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vmeste
+//
+//  Created by Александр Ломтев on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
